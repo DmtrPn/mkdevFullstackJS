49eab36c1937a74b334be473f7389095e37c7516
DmtrPn <rpsdpano@gmail.com>
2017-01-12 23:38:49 +0300

    Processing request for performances

--- backend/server.js
+++ backend/server.js
@@ -1,33 +1,15 @@
 var Koa = require('koa')
 var Router = require('koa-router')
+//var bodyParse = require('koa-body')
+var serve = require('koa-static')('../public')
 var app = new Koa()
 var router = new Router()
 
-router
-  .get('/', function *(next){
-    this.body = 'main page'
-  })
-  .get('/perfomances', function *() {
-    this.body = 'Perfomances'
-  })
-  .get('/perfomances/detail', function *() {
-    this.body = 'Perfomance detail'
-  })
-  .get('/perfomances/new', function *() {
-    this.body = 'new Perfomances'
-  })
-  .get('/users', function *() {
-    this.body = 'Users'
-  })
-  .get('/users/detail', function *() {
-    this.body = 'User detail'
-  })
-  .get('/user/new', function *() {
-    this.body = 'new User'
-  })
+var perfomances = require('./perfomances.js');
 
 app
-  .use(router.routes())
+  .use(serve)
+  .use(perfomances.routes())
   .use(router.allowedMethods())
 
-app.listen(3000)
+app.listen(8080)
